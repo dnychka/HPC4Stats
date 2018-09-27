4f200c11c9b851d4002c60aaacb5287aefea90cf
Douglas Nychka <nychka@mines.edu>
2018-09-27 14:20:33 -0600

    clean version of CMIP for cheyenne testing

--- HPC4StatsCMIP/src/getSlice.R
+++ HPC4StatsCMIP/src/getSlice.R
@@ -1,18 +1,6 @@
 
 getSlice<- function(numLat, dataFileName){
- require(ncdf4)
-#  dataFileName<- "data/pr_day_CCSM4_historical_r1i1p1_19550101-19891231.nc"
-dataHandle <- nc_open(dataFileName) # location of data file relative to working directory
-# lon<- ncvar_get(dataHandle, "lon")
-# lat<- ncvar_get(dataHandle,"lat")
-# tm<- ncvar_get(dataHandle,"time")
-timeRead<- system.time(
-dataset <- ncvar_get(dataHandle, "pr", start = c(1, numLat, 1),
-                     count=c(-1, 1,-1))
-)
-print( timeRead)
-nc_close(dataHandle)
-# NOTE: extracting just on latitude results in dataset being only 2d!
-#dimTest<- dim(dataset)
-return( dataset) 
+    load(dataFileName)
+    tempData<- prExample[,numLat,]
+    return( tempData)
 }
