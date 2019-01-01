533e44161689f03b203cdde950cae07ae08448ca
Douglas Nychka <nychka@mines.edu>
2019-01-01 14:09:23 -0700

    New Years Version

---
+++ HPC4StatsCMIP/batch/batchSupervisor.R
@@ -0,0 +1,6 @@
+
+# set the Rnamelist file name
+RNamelist <- "TEST.rnl"
+# now run the supervisor script
+# see comments in this script to follow the workflow. 
+  source("../src/HPC4StatsSupervisor.R")
